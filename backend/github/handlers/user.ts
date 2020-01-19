8a6975240409c990c38744057fe71e42763b60d7
Max Levitskiy <max@webtree.org>
2020-01-19 14:30:24 +0100

    imprint-6 Get all user's repositories

--- backend/github/handlers/user.ts
+++ backend/github/handlers/user.ts
@@ -1,10 +1,18 @@
 import {APIGatewayProxyHandler} from "aws-lambda";
+import {UserRepository} from "../repo/UserRepository";
 
 export const getRepositories: APIGatewayProxyHandler = async (event, _context) => {
     const login = event.pathParameters['login'];
-    new UserRepository().getUserRepositories(login);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({})
-    };
+    return new UserRepository()
+        .getUserRepositories(login)
+        .then(result => {
+            return {
+                statusCode: 200,
+                body: result.data
+            };
+        });
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({})
+    // };
 };
--- backend/github/repo/UserRepository.ts
+++ backend/github/repo/UserRepository.ts
@@ -1,5 +1,65 @@
-class UserRepository {
-    getUserRepositories(login: string): Repository[] {
-        return null;
+import {ApolloClient, ApolloQueryResult} from "apollo-client";
+import {gql, HttpLink, InMemoryCache, NormalizedCacheObject} from "apollo-boost";
+
+export class UserRepository {
+
+    constructor(private graphqlClient?: ApolloClient<NormalizedCacheObject>) {
+        this.graphqlClient = graphqlClient ? graphqlClient : new ApolloClient({
+            cache: new InMemoryCache(),
+            link: new HttpLink({
+                uri: 'https://api.github.com/graphql'
+            })
+        });
+    }
+
+    getUserRepositories(login: string): Promise<ApolloQueryResult<Repository[]>> {
+        return this.graphqlClient.query({
+            variables: {
+                'login': login
+            },
+            query: gql`{
+              user(login: "$login") {
+                id
+                organizations(first: 100) {
+                  nodes {
+                    repositories(first: 100) {
+                      totalCount
+                      nodes {
+                        nameWithOwner
+                        stargazers {
+                          totalCount
+                        }
+                        watchers {
+                          totalCount
+                        }
+                        isFork
+                        parent {
+                          nameWithOwner
+                        }
+                      }
+                    }
+                  }
+                }
+                repositories(first: 100) {
+                  totalCount
+                  nodes {
+                    nameWithOwner
+                    forkCount
+                    stargazers {
+                      totalCount
+                    }
+                    watchers {
+                      totalCount
+                    }
+                    isFork
+                    parent {
+                      nameWithOwner
+                    }
+                  }
+                }
+              }
+            }
+            `
+        });
     }
 }
