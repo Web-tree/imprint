db262cf8b92ad227eac4cbff0a9bad8ef668cdb4
DePr <denyspryimak@gmail.com>
2020-02-27 00:54:15 +0100

    UserDara class created

--- backend/stackexchange/stack_api.py
+++ backend/stackexchange/stack_api.py
@@ -3,6 +3,43 @@ from stackapi import StackAPI
 from stackapi import StackAPIError
 import json
 
-site = StackAPI('stackoverflow')
-users = site.fetch('users', order='desc', sort='reputation')
-print(json.dumps(users, indent=4))
+
+class UserData:
+    """ Gets the users identified in ids in {ids}.
+    Typically this method will be called to fetch user profiles when you have obtained user ids from some other source, such as /questions.
+    {ids} can contain up to 100 semicolon delimited ids. To find ids programmatically look for user_id on user or shallow_user objects.
+    The sorts accepted by this method operate on the following fields of the user object:
+    reputation – reputation
+    creation – creation_date
+    name – display_name
+    modified – last_modified_date
+    reputation is the default sort.
+    It is possible to create moderately complex queries using sort, min, max, fromdate, and todate.
+    This method returns a list of users. """
+
+    def __init__(self, user_id, **kwargs):
+        self.site = StackAPI('stackoverflow')
+        self.user_id = f'users/{user_id}'
+
+    def user_info(self):
+        return self.site.fetch(self.user_id, order='desc', sort='name')
+
+    def user_name(self):
+        return \
+            list(map(lambda x: x.get('display_name'),
+                     self.site.fetch(self.user_id, order='desc', sort='name').get('items')))[0]
+
+    def user_reputation(self):
+        return \
+            list(map(lambda x: x.get('reputation'),
+                     self.site.fetch(self.user_id, order='desc', sort='name').get('items')))[0]
+
+    @staticmethod
+    def print_data(method):
+        print(json.dumps(method, indent=4))
+
+
+# u1 = UserData('1279574;200291')
+u1 = UserData('200291')
+
+print(u1.user_name())
