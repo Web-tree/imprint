a318466a3966c8ba7c7a8be8c242f36f6dea4d67
DePr <denyspryimak@gmail.com>
2020-03-05 00:22:23 +0100

    2 classes added: 1 for api work, 2 to store data as dataclass

--- backend/stackexchange/stack_api.py
+++ backend/stackexchange/stack_api.py
@@ -2,9 +2,13 @@ from datetime import datetime
 from stackapi import StackAPI
 from stackapi import StackAPIError
 import json
+from dataclasses import dataclass
+# from dataclasses import make_dataclass
+# from dataclasses_json import dataclass_json
+from dacite import from_dict
 
 
-class UserData:
+class ApiResponse:
     """ Gets the users identified in ids in {ids}.
     Typically this method will be called to fetch user profiles when you have obtained user ids from some other source, such as /questions.
     {ids} can contain up to 100 semicolon delimited ids. To find ids programmatically look for user_id on user or shallow_user objects.
@@ -17,29 +21,56 @@ class UserData:
     It is possible to create moderately complex queries using sort, min, max, fromdate, and todate.
     This method returns a list of users. """
 
-    def __init__(self, user_id, **kwargs):
-        self.site = StackAPI('stackoverflow')
-        self.user_id = f'users/{user_id}'
+    def __init__(self, site: str, user_id: str, order: str = 'desc', sort: str = 'name') -> None:
+        self.site = StackAPI(site)
+        self.order = order
+        self.sort = sort
+        self.api_url = f'users/{user_id}'
+        self.data = self.site.fetch(self.api_url, order=self.order, sort=self.sort)
 
-    def user_info(self):
-        return self.site.fetch(self.user_id, order='desc', sort='name')
-
-    def user_name(self):
-        return \
-            list(map(lambda x: x.get('display_name'),
-                     self.site.fetch(self.user_id, order='desc', sort='name').get('items')))[0]
-
-    def user_reputation(self):
-        return \
-            list(map(lambda x: x.get('reputation'),
-                     self.site.fetch(self.user_id, order='desc', sort='name').get('items')))[0]
-
-    @staticmethod
-    def print_data(method):
-        print(json.dumps(method, indent=4))
+    def print_api_response_json(self):
+        print(json.dumps(self.data, indent=4))
 
 
-# u1 = UserData('1279574;200291')
-u1 = UserData('2335489')
+test_user = ApiResponse('stackoverflow', '2335489')
 
-u1.print_data(u1.user_info())
+
+# test_user.print_api_response_json()
+
+
+@dataclass
+class UserDataFromJSON:
+    backoff: int
+    has_more: bool
+    page: int
+    quota_max: int
+    quota_remaining: int
+    total: int
+    items: list
+
+    def __post_init__(self):
+        self.badge_counts = self.items[0].get('badge_counts')
+        self.account_id = self.items[0].get('account_id')
+        self.is_employee = self.items[0].get('is_employee')
+        self.last_modified_date = self.items[0].get('last_modified_date')
+        self.last_access_date = self.items[0].get('last_access_date')
+        self.reputation_change_year = self.items[0].get('reputation_change_year')
+        self.reputation_change_quarter = self.items[0].get('reputation_change_quarter')
+        self.reputation_change_month = self.items[0].get('reputation_change_month')
+        self.reputation_change_week = self.items[0].get('reputation_change_week')
+        self.reputation_change_day = self.items[0].get('reputation_change_day')
+        self.reputation = self.items[0].get('reputation')
+        self.creation_date = self.items[0].get('creation_date')
+        self.user_type = self.items[0].get('user_type')
+        self.user_id = self.items[0].get('user_id')
+        self.location = self.items[0].get('location')
+        self.website_url = self.items[0].get('website_url')
+        self.link = self.items[0].get('link')
+        self.profile_image = self.items[0].get('profile_image')
+        self.display_name = self.items[0].get('display_name')
+
+assert type(test_user.data) == dict
+assert len(test_user.data) == 7
+
+test_user_dataclass = from_dict(data_class=UserDataFromJSON, data=test_user.data)
+print(test_user_dataclass.reputation)
