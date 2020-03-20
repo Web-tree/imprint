21a36342a55819ebe8364b659606cee6f25d7471
DePr <denyspryimak@gmail.com>
2020-03-20 13:07:39 +0100

    finalized unittests, requirements.txt added

--- backend/stackexchange/UserRepository.py
+++ backend/stackexchange/UserRepository.py
@@ -1,6 +1,7 @@
 from stackapi import StackAPI
 from dacite import from_dict
 from backend.stackexchange.User import *
+from dat
 
 
 class UserRepository:
--- backend/stackexchange/test_UserRepository.py
+++ backend/stackexchange/test_UserRepository.py
@@ -11,46 +11,47 @@ Test data:
 '''
 
 
-class TestStackApi(unittest.TestCase):
+class TestUserRepository(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def test_stackoverflow(self):
         self.user_stackoverflow = UserRepository('stackoverflow').get_user('2335489')
-        self.user_superuser = UserRepository('superuser').get_user('633364')
-        self.user_askubuntu = UserRepository('askubuntu').get_user('398808')
-        self.user_unix = UserRepository('unix').get_user('22565')
-        self.user_serverfault = UserRepository('serverfault').get_user('201277')
-
-    def test_User_object(self):
         self.assertTrue(isinstance(self.user_stackoverflow.account_id, int))
-        self.assertTrue(isinstance(self.user_superuser.user_id, int))
-        self.assertTrue(isinstance(self.user_askubuntu.reputation, int))
-        self.assertTrue(isinstance(self.user_unix.link, str))
-        self.assertTrue(isinstance(self.user_serverfault.display_name, str))
-
         self.assertEqual(self.user_stackoverflow.account_id, 2705879)
         self.assertEqual(self.user_stackoverflow.user_id, 2335489)
         self.assertIn(self.user_stackoverflow.reputation, range(44, 64))
         self.assertTrue(self.user_stackoverflow.link, self.user_stackoverflow.link.startswith('https'))
         self.assertEqual(self.user_stackoverflow.display_name, 'LucemFerre')
 
+    def test_superuser(self):
+        self.user_superuser = UserRepository('superuser').get_user('633364')
+        self.assertTrue(isinstance(self.user_superuser.user_id, int))
         self.assertEqual(self.user_superuser.account_id, 1030062)
         self.assertEqual(self.user_superuser.user_id, 633364)
         self.assertIn(self.user_superuser.reputation, range(100, 160))
         self.assertTrue(self.user_superuser.link, self.user_superuser.link.startswith('https'))
         self.assertEqual(self.user_superuser.display_name, 'Beto Aveiga')
 
+    def test_askubuntu(self):
+        self.user_askubuntu = UserRepository('askubuntu').get_user('398808')
+        self.assertTrue(isinstance(self.user_askubuntu.reputation, int))
         self.assertEqual(self.user_askubuntu.account_id, 3059843)
         self.assertEqual(self.user_askubuntu.user_id, 398808)
         self.assertIn(self.user_askubuntu.reputation, range(1000, 1500))
         self.assertTrue(self.user_askubuntu.link, self.user_askubuntu.link.startswith('https'))
         self.assertEqual(self.user_askubuntu.display_name, 'Bernard Wei')
 
+    def test_unix(self):
+        self.user_unix = UserRepository('unix').get_user('22565')
+        self.assertTrue(isinstance(self.user_unix.link, str))
         self.assertEqual(self.user_unix.account_id, 1744646)
         self.assertEqual(self.user_unix.user_id, 22565)
         self.assertIn(self.user_unix.reputation, range(360000, 400423))
         self.assertTrue(self.user_unix.link, self.user_unix.link.startswith('https'))
         self.assertEqual(self.user_unix.display_name, 'St&#233;phane Chazelas')
 
+    def test_serverfault(self):
+        self.user_serverfault = UserRepository('serverfault').get_user('201277')
+        self.assertTrue(isinstance(self.user_serverfault.display_name, str))
         self.assertEqual(self.user_serverfault.account_id, 3396486)
         self.assertEqual(self.user_serverfault.user_id, 201277)
         self.assertIn(self.user_serverfault.reputation, range(5, 25))
